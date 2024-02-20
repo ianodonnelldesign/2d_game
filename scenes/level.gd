6e3fc0272a811430a5fe968da1e3589ae4f60869
Tenatian <ianodonnellk@gmail.com>
2024-02-19 23:30:19 -0500

    Some door edits

--- scenes/level.gd
+++ scenes/level.gd
@@ -5,6 +5,7 @@ func _ready():
 		on_level_spawn(NavigationManager.spawn_door_tag)
 
 func on_level_spawn(destination_door_tag: String):
-	var door_path = "$Doors/" + destination_door_tag
-	var door = get_node("Doors/Door_F1") as Door
-	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
+	if destination_door_tag != "":
+		var door_path = "Doors/" + destination_door_tag
+		var door = get_node(door_path) as Door
+		NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
--- scripts/navigation_manager.gd
+++ scripts/navigation_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var spawn_door_tag : String
+@onready var spawn_door_tag : String
 signal on_trigger_player_spawn
 
 func go_to_level(destination_level_tag, destination_door_tag):
